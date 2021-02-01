a4df14150447b0d7461d94ea76e6447a1b3ac751
AlexDraft <lightdraft89@gmail.com>
2021-02-01 11:56:38 +0700

    C1.8

---
+++ C1.8/main.py
@@ -0,0 +1,12 @@
+from cat import Cat
+
+cat1 = Cat("Барон", "Мальчик", 2)
+cat2 = Cat("Сэм", "Мальчик", 2)
+
+print("cat1.name=", cat1.name)
+print("cat1.sex=", cat1.sex)
+print("cat1.age=", cat1.age, "года")
+
+print("cat2.name=", cat2.name)
+print("cat2.sex=", cat2.sex)
+print("cat2.age=", cat2.age, "года")
